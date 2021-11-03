e5032b124bd8d40f8835007c6819dbb9adceb6d9
Ariane Bonnel <abonnel@z3r2p3.42lyon.fr>
2021-11-03 10:37:44 +0100

    added Makefile

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abonnel <abonnel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 10:26:44 by abonnel           #+#    #+#             */
+/*   Updated: 2021/11/03 10:37:05 by abonnel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string>
 
@@ -5,7 +17,7 @@ int main(int argc, char **argv)
 {
     int         i = 1;
     int         y = 0;
-    std::string arg;
+    std::string megaphoned_string;
 
     if (argc < 2)
     {
@@ -17,11 +29,11 @@ int main(int argc, char **argv)
         y = 0;
         while (argv[i][y])
         {
-            arg += toupper(argv[i][y]);
+            megaphoned_string += toupper(argv[i][y]);
             y++;
         }
         i++;
     }
-    std::cout << "string = " << arg << std::endl;
+    std::cout << megaphoned_string << std::endl;
     return 0;
 }
