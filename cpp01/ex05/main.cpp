64e54a1fee97742b29d34d47df030537492384a2
Ariane Bonnel <abonnel@z3r2p3.42lyon.fr>
2021-11-17 13:57:44 +0100

    added newlines for clarity

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,21 +6,27 @@
 /*   By: abonnel <abonnel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 12:58:19 by abonnel           #+#    #+#             */
-/*   Updated: 2021/11/17 13:54:05 by abonnel          ###   ########.fr       */
+/*   Updated: 2021/11/17 13:57:18 by abonnel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
+#include <iostream>
 
 int main()
 {
 	Karen	Karen;
 	
-	Karen.complain("DEBUG");
 	Karen.complain("INFO");
+	std::cout << std::endl;
+	Karen.complain("DEBUG");
+	std::cout << std::endl;
 	Karen.complain("WARNING");
+	std::cout << std::endl;
 	Karen.complain("ERROR");
+	std::cout << std::endl;
 	Karen.complain("WARNING");
+	std::cout << std::endl;
 	Karen.complain("WARNING ");
-	
+	return (0);
 }
