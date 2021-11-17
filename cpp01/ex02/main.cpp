2eef69e5ba2042cc4b697bc58889afddfd58b230
Ariane Bonnel <abonnel@z3r2p3.42lyon.fr>
2021-11-17 14:53:26 +0100

    Makefile tracks includes

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ariane <ariane@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abonnel <abonnel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 17:40:59 by ariane            #+#    #+#             */
-/*   Updated: 2021/11/10 17:53:32 by ariane           ###   ########.fr       */
+/*   Updated: 2021/11/17 14:48:41 by abonnel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int main()
     std::string     str = "HI THIS IS BRAIN";
     std::string     *str_ptr = &str;
     std::string     &str_ref = str;
-    char            *ptr = &str[0];
     
     std::cout << "String address :\t" << &str << std::endl;
     std::cout << "String address by ptr :\t" << str_ptr << std::endl;
