8f2376152ee9d184e8e936f0e0a57a8d666c4401
Ariane Bonnel <abonnel@z3r2p3.42lyon.fr>
2021-12-01 12:25:29 +0100

    ex02

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -6,13 +6,17 @@
 /*   By: abonnel <abonnel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 11:13:12 by ariane            #+#    #+#             */
-/*   Updated: 2021/11/29 14:49:18 by abonnel          ###   ########.fr       */
+/*   Updated: 2021/12/01 12:24:59 by abonnel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 #include <cmath>
+
+/*
+** CONSTRUCTOR DESTRUCTOR =====================================================
+*/
 
 Fixed::Fixed()
 {
@@ -40,6 +44,166 @@ Fixed::Fixed(const float given_float)
 	this->_value = (round(given_float * (1 << this->_point_position)));
 }
 
+
+Fixed &     Fixed::operator=(Fixed const &to_copy)
+{
+    this->_value = to_copy.getRawBits();
+    return (*this);   
+}
+
+/*
+** ARITHMETIC OPERATIONS + - * / ==============================================
+*/
+
+Fixed Fixed::operator+(Fixed const &nb) const
+{
+	Fixed		tmp;
+
+	tmp._value = this->_value + nb._value;
+	return tmp;
+}
+
+Fixed Fixed::operator-(Fixed const &nb) const
+{
+	Fixed		tmp;
+
+	tmp._value = this->_value - nb._value;
+	return tmp;
+}
+
+//verify it works
+Fixed Fixed::operator*(Fixed const &nb) const
+{
+	Fixed		tmp;
+
+	tmp._value = this->_value * nb._value / (1 << this->_point_position);
+	return (tmp);
+}
+
+Fixed Fixed::operator/(Fixed const &nb) const
+{
+	Fixed		tmp(this->toFloat() / nb.toFloat());
+
+	return (tmp);
+}
+
+/*
+** COMPARISON OPERATORS >, <, >=, <=, == and != ===============================
+*/
+
+bool Fixed::operator<(Fixed const &nb) const
+{
+	if (this->_value < nb._value)
+		return true;
+	return false;
+}
+
+bool Fixed::operator<=(Fixed const &nb) const
+{
+	if (this->_value <= nb._value)
+		return true;
+	return false;
+}
+
+bool Fixed::operator>=(Fixed const &nb) const
+{
+	if (this->_value >= nb._value)
+		return true;
+	return false;
+}
+
+bool Fixed::operator>(Fixed const &nb) const
+{
+	if (this->_value > nb._value)
+		return true;
+	return false;
+}
+
+bool Fixed::operator==(Fixed const &nb) const
+{
+	if (this->_value == nb._value)
+		return true;
+	return false;
+}
+
+bool Fixed::operator!=(Fixed const &nb) const
+{
+	if (this->_value != nb._value)
+		return true;
+	return false;
+}
+
+/*
+** DECREMENT INCREMENT ========================================================
+*/
+
+Fixed &Fixed::operator++()
+{
+	_value++;
+	return (*this);
+}
+
+Fixed Fixed::operator++(int)
+{
+	Fixed tmp;
+	
+	tmp = *this;
+	_value++;
+	return (tmp);
+}
+
+Fixed &Fixed::operator--()
+{
+	_value--;
+	return (*this);
+}
+
+Fixed Fixed::operator--(int)
+{
+	Fixed tmp;
+	
+	tmp = *this;
+	_value--;
+	return (tmp);
+}
+
+/*
+** MIN MAX - FUN OVERLOADING ==================================================
+*/
+
+Fixed& Fixed::min(Fixed &a, Fixed &b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+Fixed const& Fixed::min(Fixed const &a, Fixed const &b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+Fixed& Fixed::max(Fixed &a, Fixed &b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+Fixed const& Fixed::max(Fixed const &a, Fixed const &b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+
+/*
+** MEMBER FUNCTIONS  ==========================================================
+*/
+
 int Fixed::toInt(void) const
 {
 	return (this->_value >> 8);
@@ -48,45 +212,6 @@ int Fixed::toInt(void) const
 float Fixed::toFloat(void) const
 {
 	return ((float)this->_value / (float)(1 << this->_point_position));
-}
-
-Fixed &     Fixed::operator=(Fixed const &to_copy)
-{
-    this->_value = to_copy.getRawBits();
-    return (*this);   
-}
-
-Fixed Fixed::operator+(Fixed const &to_copy) const
-{
-	Fixed		tmp;
-
-	tmp._value = this->_value + to_copy._value;
-	return tmp;
-}
-
-Fixed Fixed::operator-(Fixed const &to_copy) const
-{
-	Fixed		tmp;
-
-	tmp._value = this->_value - to_copy._value;
-	return tmp;
-}
-
-Fixed Fixed::operator*(Fixed const &to_copy) const
-{
-	Fixed		tmp;
-
-	tmp._value = this->_value * to_copy._value / (1 << this->_point_position);
-	return (tmp);
-}
-
-Fixed Fixed::operator/(Fixed const &to_copy) const
-{
-	Fixed		tmp;
-
-	tmp._value = (this->_value << this->_point_position) / (to_copy._value << this->_point_position);
-	tmp._value = tmp._value >> this->_point_position;
-	return (tmp);
 }
 
 int Fixed::getRawBits(void) const
@@ -98,6 +223,10 @@ void Fixed::setRawBits(int const raw)
 {
     this->_value = raw;
 }
+
+/*
+** OPERATOR << ================================================================
+*/
 
 std::ostream & operator<<(std::ostream &cout, Fixed const &fixed)
 {
--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: abonnel <abonnel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 11:10:11 by ariane            #+#    #+#             */
-/*   Updated: 2021/11/29 14:58:21 by abonnel          ###   ########.fr       */
+/*   Updated: 2021/12/01 12:20:16 by abonnel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,33 @@ class   Fixed
     Fixed(const Fixed &to_copy);
     Fixed(const int given_int);
     Fixed(const float given_float);
+    ~Fixed();
+	
+    Fixed &operator=(Fixed const &to_copy);
+    Fixed operator+(Fixed const &nb) const;
+    Fixed operator-(Fixed const &nb) const;
+    Fixed operator*(Fixed const &nb) const;
+    Fixed operator/(Fixed const &nb) const;
+	
+	bool operator<(Fixed const &nb) const;
+	bool operator<=(Fixed const &nb) const;
+	bool operator>=(Fixed const &nb) const;
+	bool operator>(Fixed const &nb) const;
+	bool operator==(Fixed const &nb) const;
+	bool operator!=(Fixed const &nb) const;
+
+	Fixed &operator++();
+	Fixed operator++(int);
+	Fixed &operator--();
+	Fixed operator--(int);
+
+	static Fixed& min(Fixed &a, Fixed &b);
+	static Fixed const& min(Fixed const &a, Fixed const &b);
+	static Fixed& max(Fixed &a, Fixed &b);
+	static Fixed const& max(Fixed const &a, Fixed const &b);
+	
 	float toFloat(void) const;
 	int toInt(void) const;
-    Fixed &operator=(Fixed const &to_copy);
-    Fixed operator+(Fixed const &to_copy) const;
-    Fixed operator-(Fixed const &to_copy) const;
-    Fixed operator*(Fixed const &to_copy) const;
-    Fixed operator/(Fixed const &to_copy) const;//not working
-    ~Fixed();
     int getRawBits(void) const;
     void setRawBits(int const raw);
 
