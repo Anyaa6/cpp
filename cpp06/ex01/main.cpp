e074873813b0a79443db9fe7db827dfc36caf663
Ariane Bonnel <abonnel@z3r2p3.42lyon.fr>
2021-12-21 16:07:45 +0100

    ex01

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,10 +6,37 @@
 /*   By: abonnel <abonnel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 10:32:46 by abonnel           #+#    #+#             */
-/*   Updated: 2021/12/21 10:39:31 by abonnel          ###   ########.fr       */
+/*   Updated: 2021/12/21 16:07:12 by abonnel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdint>
-//https://www.cplusplus.com/reference/cstdint/
-//if p is a valid pointer, then (void *)p == (void *)(uintptr_t)p
+#include <iostream>
+
+struct Data {
+	int random;	
+};
+
+uintptr_t serialize(Data* ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data *>(raw));
+}
+
+int main()
+{
+	Data	instance;
+	Data	*instanceptr;
+	
+	instance.random = 12;
+	instanceptr = deserialize(serialize(&instance));
+	std::cout << "instance.random = " << instanceptr->random << std::endl;
+
+	if (&instance == instanceptr)
+		std::cout << "Pointers are equal" << std::endl;
+	return 0;
+}
