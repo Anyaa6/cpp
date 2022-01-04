5b1111358971f7acc5bf405452d505866ee0f14f
Ariane <ariane@iMac-de-Ariane.local>
2022-01-04 15:14:08 +0100

    ex01 clean main

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -8,23 +8,28 @@
 int main()
 {
 	int array[] = {1, 5, 7, 8};
-	iter<int>(array, 4, &(add_2<int>));
+
+	std::cout << "Before iter" << std::endl;
 	for (int i = 0; i < 4; i++)
-	{
 		std::cout << "array[" << i << "] = " << array[i] << std::endl;
-	}
+
+	iter<int>(array, 4, &(add_2<int>));
+	std::cout << "\nAfter iter" << std::endl;
+	for (int i = 0; i < 4; i++)
+		std::cout << "array[" << i << "] = " << array[i] << std::endl;
 	std::cout << std::endl;
 
 	float otherArray[] = {8.2, 4.7, 7.3, 9.0};
+
+	std::cout << "Before iter" << std::endl;
 	for (int i = 0; i < 4; i++)
-	{
 		std::cout << "otherarray[" << i << "] = " << otherArray[i] << std::endl;
-	}
 	std::cout << std::endl;
 
 	iter<float>(otherArray, 4, &(add_2<float>));
-
+	std::cout << "\nAfter iter" << std::endl;
 	for (int i = 0; i < 4; i++) {
 		std::cout << "otherarray[" << i << "] = " << otherArray[i] << std::endl;
 	}
+	return 0;
 }
