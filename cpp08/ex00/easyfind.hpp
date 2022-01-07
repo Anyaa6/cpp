e75baf1bf1735cbcfc0d621944fbc93d8fa19b7f
Ariane <ariane@iMac-de-Ariane.local>
2022-01-07 15:55:07 +0100

    ex00 done

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -4,22 +4,13 @@
 
 #ifndef CPP08_EASYFIND_HPP
 #define CPP08_EASYFIND_HPP
-/*
-Make a template function called easyfind, templated on a type T, that takes a T
-and an int.
-Assume the T is a container of int, and find the first occurrence of the second parameter in the first parameter.
-If it can’t be found, handle the error either using an exception or using an error return
-value. Take ideas from how standard containers work.
-Of course, you will provide a main function that tests it thoroughly.
- */
+
 #include <algorithm>
 
 template <typename T>
-T & easyfind(T &haystack, int needle)
+typename T::iterator easyfind(T &haystack, int needle)
 {
 	return (find(haystack.begin(), haystack.end(), needle));
 }
-
-
 
 #endif //CPP08_EASYFIND_HPP
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -4,8 +4,36 @@
 
 #include "easyfind.hpp"
 #include <vector>
+#include <iostream>
 
 int main()
 {
-	vector<int> array(12, 2);
+	std::cout << "Created a vector of 12 ints, they all have value 2 except one "
+				 "that has value 15" << std::endl;
+	std::vector<int> array(12, 2);
+	std::vector<int>::iterator it;
+	int needle;
+	array[3] = 15;
+
+	needle = 2;
+	it = easyfind(array, needle);
+	if (it == array.end())
+		std::cout << "value " << needle << " not found" << std::endl;
+	else
+		std::cout << "value " << needle << " found" << std::endl;
+
+	needle = 3;
+	it = easyfind(array, needle);
+	if (it == array.end())
+		std::cout << "value " << needle << " not found" << std::endl;
+	else
+		std::cout << "value " << needle << " found" << std::endl;
+
+	needle = 15;
+	it = easyfind(array, needle);
+	if (it == array.end())
+		std::cout << "value " << needle << " not found" << std::endl;
+	else
+		std::cout << "value " << needle << " found" << std::endl;
+
 }
